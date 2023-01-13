0283e6a2ef2c101fa828414badb15a5b68dc1ebd
Roberto Alves <roberto@rhfactor.com.br>
2023-01-12 23:25:15 -0300

    adicionar component de login

---
+++ src/views/LoginView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+
+    Aqui inicial
+
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  name: 'LoginView',
+
+});
+</script>
