44de449cee5bb404509b13d42133b62629c37477
Roberto Alves <roberto@rhfactor.com.br>
2023-01-14 11:35:52 -0300

    Usando Vee-Validate

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,35 +1,53 @@
 <template>
-  <form>
-
+  <Form v-slot="{ values }" @submit="onSubmit" :validation-schema="schema" :initial-values="formValues">
     <div class="mb-3">
       <label for="username" class="form-label">Email</label>
-      <input type="email" class="form-control" id="username" placeholder="email@example.com">
+      <Field id="username" name="username" class="form-control" type="email" />
     </div>
     <div class="mb-3">
       <label for="password" class="form-label">Senha</label>
-      <input type="text" class="form-control" id="password">
+      <Field name="password" class="form-control" type="password" />
     </div>
-    <div class="mb-3">
-      <div class="d-grid gap-2">
-        <button class="btn btn-primary" type="button">Login</button>
-      </div>
+    <pre>{{ values }}</pre>
+    <div class="d-grid gap-2 mb-3">
+      <button class="btn btn-primary" type="submit">Login</button>
     </div>
-
-  </form>
+  </Form>
 </template>
 
 <script lang="ts">
-import {defineComponent} from "vue";
+import {Field, Form} from 'vee-validate'
+import * as yup from 'yup';
+import {defineComponent} from "vue"
+
+const schema = yup.object({
+  username: yup.string().required().email(),
+  password: yup.string().required().min(8),
+});
+
+const formValues = {
+  username: '',
+  password: '',
+}
 
 export default defineComponent({
   name: 'LoginForm',
+  components: {
+    Form,
+    Field,
+  },
   data() {
     return {
-      message: "Mensagem"
+      schema : schema,
+      formValues: formValues,
+    }
+  },
+  methods:{
+    onSubmit( values : any ){
+      console.log( JSON.stringify( values ) )
     }
   }
-});
-
+})
 </script>
 
 <style scoped>
