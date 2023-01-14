4297a42708991f5cce75641d0bf488a8b3db1000
Roberto Alves <roberto@rhfactor.com.br>
2023-01-14 10:12:54 -0300

    Instalacao Boostrap e Fontawesome

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
+// FontAwesome
+import { FontAwesomeIcon } from '@/plugins/font-awesome'
+
+createApp(App)
+    .use(store)
+    .use(router)
+    .component("font-awesome-icon", FontAwesomeIcon)
+    .mount('#app')
